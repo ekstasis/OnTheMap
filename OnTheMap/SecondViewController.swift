edc5695f2be6d0355b2243c58ae4ca2f790fce73
ekstasis <straightstory@gmail.com>
2015-11-08 14:08:20 -0500

    Initial Commit

---
+++ OnTheMap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OnTheMap
+//
+//  Created by Ekstasis on 11/8/15.
+//  Copyright © 2015 Baxter Heavy Industries. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
