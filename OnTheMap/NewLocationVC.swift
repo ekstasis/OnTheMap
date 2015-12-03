7709dbe02d1f4338270ac31575a9ea05f9c16d6d
ekstasis <straightstory@gmail.com>
2015-12-03 08:47:24 -0500

    Keep fiddling with new location storyboard

--- OnTheMap/NewLocationVC.swift
+++ OnTheMap/NewLocationVC.swift
@@ -7,8 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
-class NewLocationVC: UIViewController {
+class NewLocationVC: UIViewController, UITextViewDelegate {
     
-   
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var locationTextView: UITextView!
+    
+    var locationText: String!
+    var locationCoords: CLLocationCoordinate2D!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pressedFindLocation(sender: UIButton) {
+        locationText = locationTextView.text
+        // geocode it
+        // present error if necessary
+        // present next views
+        stackView.hidden = true
+    }
 }
