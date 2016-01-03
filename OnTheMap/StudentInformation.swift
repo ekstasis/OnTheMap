d5ed7e39fe681efc759df59c3d8fac8a62a1f9d5
ekstasis <straightstory@gmail.com>
2016-01-03 00:04:06 -0500

    Weird, these files weren't added with git commit -a

---
+++ OnTheMap/StudentInformation.swift
@@ -0,0 +1,57 @@
+//
+//  StudentInformation.swift
+//  OnTheMap
+//
+//  Created by Ekstasis on 11/10/15.
+//  Copyright © 2015 Baxter Heavy Industries. All rights reserved.
+//
+
+import Foundation
+
+struct StudentInformation {
+  
+  static var studentLocations: [StudentInformation]!
+  
+  let createdAt: String // "2015-02-24T22:35:30.639Z",
+  let firstName: String // "John",
+  let lastName: String // "Doe",
+  let latitude: Double // 37.322998,
+  let longitude: Double // -122.032182,
+  let mediaURL: String // "https://udacity.com",
+  let objectID: String // "8ZEuHF5uX8",
+  let userID: String // "1234",
+  let updatedAt: String // "2015-03-11T02:42:59.217Z"
+  
+  var fullName: String {
+    return "\(firstName) \(lastName)"
+  }
+  
+  init(location: [String: AnyObject]) {
+    createdAt = location["createdAt"] as! String
+    firstName = location["firstName"] as! String
+    lastName = location["lastName"] as! String
+    latitude = location["latitude"] as! Double
+    longitude = location["longitude"] as! Double
+    mediaURL = location["mediaURL"] as! String
+    objectID = location["objectId"] as! String
+    userID = location["uniqueKey"] as! String
+    updatedAt = location["updatedAt"] as! String
+  }
+  
+  // Generates array of StudentInformation structs from dictionary
+  static func arrayFromJSON(json: [String: AnyObject], completion: (locationsResult: [StudentInformation]?, errorString: String?) -> Void) {
+    
+    var studentLocations = [StudentInformation]()
+    
+    if let locations = json["results"] as? [[String: AnyObject]] {
+      for location in locations {
+        studentLocations.append(StudentInformation(location: location))
+      }
+      
+      completion(locationsResult: studentLocations, errorString: nil)
+      
+    } else {
+      completion(locationsResult: nil, errorString: "An error occurred while parsing location data")
+    }
+  }
+}
